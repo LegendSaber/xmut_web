058b36fb505b0a7b451df630849d228b45aeacd3
1900 <747671402@qq.com>
2020-01-29 11:05:56 +0800

    项目初始化

---
+++ src/utils/http.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export function http(config) {
+    const instance = axios.create({
+        baseURL: '',
+        timeout: 5000
+    })
+
+    instance.interceptors.request.use(config => {
+        return config;
+    }, err => {
+
+    })
+
+    instance.interceptors.response.use(res => {
+        return res.data;
+    }, err => {
+        console.log(err);
+    })
+
+    return instance(config)
+}
