ef3a07a2019501d665b11c2e135b76da8a6ba95d
1900 <747671402@qq.com>
2020-02-16 22:55:06 +0800

    收藏功能的实现以及论坛页面的分类效果的实现

--- src/views/sys/experience/detail/module/main.vue
+++ src/views/sys/experience/detail/module/main.vue
@@ -1,23 +1,54 @@
 <template>
-    <div>
-        <h1>{{essay}}</h1>
-    </div>
+  <div>
+    <h1>{{essay}}</h1>
+    <el-button v-if="!isCollect" @click="collect" type="primary" icon="el-icon-star-off" plain>点击收藏</el-button>
+    <el-button v-else @click="cancelCollect" type="warning" icon="el-icon-star-on">已收藏</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            essay: {}
+  data() {
+    return {
+      essay: {},
+      isCollect: false
+    };
+  },
+  methods:{
+      collect() {
+          let params = {}
+          params.id = this.$data.essay.id
+          this.$axios.post("/collect/collectEx", params).then(response => {
+              if (response && response.success){
+                  this.$data.isCollect = true
+              }
+          })
+      },
+      cancelCollect() {
+          let params = {}
+          params.id = this.$data.essay.id
+          this.$axios.post("/collect/cancelCollectEx", params).then(response => {
+              if (response && response.success){
+                  this.$data.isCollect = false
+              }
+          })
+      }
+  },
+  created() {
+    if (this.$route.query.content.id == null) this.goBack();
+    this.$data.essay = this.$route.query.content;
+    let params = {};
+    params.id = this.$data.essay.id
+    this.$axios.post("/collect/getExCollect", params).then(response =>{
+        if (response && response.success){
+            this.$data.isCollect = true
+        }else {
+            this.$data.isCollect = false
         }
-    },
-    created() {
-      if (this.$route.query.content.id == null) this.goBack()
-      this.$data.essay = this.$route.query.content
-    }
-}
+    })
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/views/sys/experience/index.vue
+++ src/views/sys/experience/index.vue
@@ -32,11 +32,19 @@
             <el-input :autosize="{maxRows: 1}" v-model="ruleForm.title"></el-input>
           </el-form-item>
           <el-form-item label="内容" prop="content">
-            <el-input :autosize="{minRows:30,maxRows: 40}" type="textarea" v-model="ruleForm.content"></el-input>
+            <el-input
+              :autosize="{minRows:30,maxRows: 40}"
+              type="textarea"
+              v-model="ruleForm.content"
+            ></el-input>
           </el-form-item>
           <el-form-item>
             <div class="demo-drawer__footer">
-              <el-button :loading="loading" type="primary" @click="submitForm('ruleForm')">{{ loading ? '提交中 ...' : '提交' }}</el-button>
+              <el-button
+                :loading="loading"
+                type="primary"
+                @click="submitForm('ruleForm')"
+              >{{ dwloading ? '提交中 ...' : '提交' }}</el-button>
               <el-button type="warning" @click="resetForm('ruleForm')" plain>重置</el-button>
               <el-button @click="cancelForm" type="danger" plain>取 消</el-button>
             </div>
@@ -78,11 +86,13 @@ export default {
       tableData: [],
       loading: true,
       queryData: {
+        isScroll: false,
+        flag: 1,
         currentPage: 0,
         pageSize: 12
       },
       dialog: false,
-      loading: false,
+      dwloading: false,
       ruleForm: {
         title: "",
         content: ""
@@ -113,67 +123,148 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
-          let params = {}
-          this.loading = true
-          params.title = this.$data.ruleForm.title
-          params.content = this.$data.ruleForm.content
-          setTimeout(()=>{
-            this.$axios.post("/sysExperience/insert", params).then(response=>{
-              this.loading = false
+          let params = {};
+          this.$data.dwloading = true;
+          params.title = this.$data.ruleForm.title;
+          params.content = this.$data.ruleForm.content;
+          setTimeout(() => {
+            this.$axios.post("/sysExperience/insert", params).then(response => {
+              this.$data.dwloading = false;
               if (response && response.success) {
-                this.$alert(response.message, '提交结果', {
-                confirmButtonText: '确定',
+                this.$alert(response.message, "提交结果", {
+                  confirmButtonText: "确定",
                   callback: action => {
-                    this.resetForm('ruleForm')
-                    this.$data.loading = false
-                    this.$data.dialog = false
+                    this.resetForm("ruleForm");
+                    this.$data.dialog = false;
                   }
-                })
+                });
               } else {
-                this.$notify.error(response.message)
-                return false
+                this.$notify.error(response.message);
+                return false;
               }
-            })
-          }, 2000)
+            });
+          }, 2000);
         } else {
-          this.$alert('表单信息填写有误，请修改!', '提交结果', {
-            confirmButtonText: '确定',
+          this.$alert("表单信息填写有误，请修改!", "提交结果", {
+            confirmButtonText: "确定",
             callback: action => {}
-          })
-          return false
+          });
+          return false;
         }
-      })
+      });
     },
     resetForm(formName) {
-      this.$refs[formName].resetFields()
+      this.$refs[formName].resetFields();
     },
     handleClose(done) {
-      if (this.$data.loading) {
+      if (this.$data.dwloading) {
         return;
       }
-      this.$confirm("确定要关闭表单吗？")
-        .then(_ => {
-          this.resetForm('ruleForm')
-          done()
-        })
+      this.$confirm("确定要关闭表单吗？").then(_ => {
+        this.resetForm("ruleForm");
+        done();
+      });
     },
     cancelForm() {
-      this.resetForm('ruleForm')
-      this.$data.loading = false;
+      this.resetForm("ruleForm");
+      this.$data.dwloading = false;
       this.$data.dialog = false;
     },
     categoryHandle() {
+      if (this.$data.loading) 
+      {
+        this.$data.categoryName = this.$data.oldCategory;
+        return;
+      }
       if (this.$data.oldCategory != this.$data.categoryName) {
+        this.$data.queryData.currentPage = 0;
+        this.$data.queryData.pageSize = 12;
+        this.$data.queryData.isScroll = false;
+        this.$data.loading = true;
+        window.addEventListener("scroll", this.windowScroll);
         if (this.$data.categoryName === "new") {
+          this.$data.queryData.flag = 1;
         } else if (this.$data.categoryName === "hot") {
+          this.$data.queryData.flag = 2;
         } else if (this.$data.categoryName === "collect") {
+          this.$data.queryData.flag = 3;
         } else {
+          this.$data.queryData.flag = 4;
         }
-        this.$data.oldCategory = this.$data.categoryName;
+        setTimeout(() => {
+          document.documentElement.scrollTop = 0;
+          window.pageYOffset = 0;
+          document.body.scrollTop = 0;
+          this.getExperienceData(this.$data.queryData.flag);
+          this.$data.oldCategory = this.$data.categoryName;
+          this.$data.loading = false;
+        }, 2000);
+      }
+    },
+    getExperienceData(flag) {
+      let params = {};
+      params.currentPage = this.$data.queryData.currentPage;
+      params.pageSize = this.$data.queryData.pageSize;
+
+      if (flag == 1 || flag == 2) {
+        params.flag = flag;
+        this.$axios.post("/sysExperience/getAll", params).then(response => {
+          if (response && response.success) {
+            this.setData(response.data);
+          }
+        });
+      } else if (flag == 3) {
+        this.$axios
+          .post("/sysExperience/getFavorExperience", params)
+          .then(response => {
+            if (response && response.success) {
+              this.setData(response.data);
+            }
+          });
+      } else if (flag == 4) {
+        this.$axios
+          .post("/sysExperience/getMyExperience", params)
+          .then(response => {
+            if (response && response.success) {
+              this.setData(response.data);
+            }
+          });
+      }
+    },
+    setData(data) {
+      let tmpData = data.records;
+      let size = tmpData.length;
+
+      if (this.$data.queryData.isScroll) {
+        for (let i = 0; i < size; i++) {
+          tmpData[i].modifyTime = tmpData[i].modifyTime.slice(0, 10);
+          this.$data.tableData.push(tmpData[i]);
+        }
+        if (size < 2) {
+          this.$notify.error("没有更多数据");
+          window.removeEventListener("scroll", this.windowScroll);
+        } else {
+          this.$data.queryData.currentPage++;
+        }
+      } else {
+        this.$data.tableData = tmpData;
+        let length = this.$data.tableData.length;
+
+        for (let i = 0; i < length; i++) {
+          this.$data.tableData[i].modifyTime = this.$data.tableData[
+            i
+          ].modifyTime.slice(0, 10);
+        }
+
+        if (size < this.$data.queryData.pageSize) {
+          window.removeEventListener("scroll", this.windowScroll);
+        }
+        this.$data.queryData.currentPage = size / 2 + 1;
+        this.$data.queryData.pageSize = 2;
       }
     },
     show(row) {
-      this.$router.push({path: '/exdetail', query: {content: row}})
+      this.$router.push({ path: "/exdetail", query: { content: row } });
     },
     windowScroll() {
       //滚动条滚动时距离顶部的距离
@@ -189,30 +280,13 @@ export default {
         document.documentElement.scrollHeight || document.body.scrollHeight;
 
       if (scrollTop + windowHeight == scrollHeight) {
-        let params = {};
-        params.currentPage = this.$data.queryData.currentPage;
-        params.pageSize = this.$data.queryData.pageSize;
-
+        if (this.$data.loading) return;
         this.$data.loading = true;
         setTimeout(() => {
-          this.$axios.post("/sysExperience/getAll", params).then(response => {
-            if (response && response.data) {
-              let data = response.data.records;
-
-              if (data.length == 0) {
-                this.$notify.error("没有更多数据");
-                window.removeEventListener("scroll", this.windowScroll);
-              } else{
-                data[0].modifyTime = data[0].modifyTime.slice(0, 10)  
-                this.$data.tableData.push(data[0]);
-                if (data.length == 2) {
-                  data[1].modifyTime = data[1].modifyTime.slice(0, 10)
-                  this.$data.tableData.push(data[1])
-                }
-                this.$data.queryData.currentPage++
-              }
-            }
-          });
+          document.documentElement.scrollTop = scrollTop;
+          console.log(document.documentElement.scrollTop);
+          this.$data.queryData.isScroll = true;
+          this.getExperienceData(this.$data.queryData.flag);
           this.$data.loading = false;
         }, 2000);
       }
@@ -222,26 +296,10 @@ export default {
     window.addEventListener("scroll", this.windowScroll);
   },
   created() {
-    let params = {};
-    params.currentPage = this.$data.queryData.currentPage;
-    params.pageSize = this.$data.queryData.pageSize;
-    this.$axios.post("/sysExperience/getAll", params).then(response => {
-      if (response && response.data) {
-        this.$data.tableData = response.data.records;
-        let length = this.$data.tableData.length;
-
-        for (let i = 0; i < length; i++) {
-          this.$data.tableData[i].modifyTime = this.$data.tableData[
-            i
-          ].modifyTime.slice(0, 10);
-        }
-
-        this.$data.queryData.currentPage =
-          this.$data.queryData.pageSize / 2 + 1;
-        this.$data.queryData.pageSize = 2;
-        this.$data.loading = false;
-      }
-    });
+    setTimeout(() => {
+      this.getExperienceData(this.$data.queryData.flag);
+      this.$data.loading = false;
+    }, 2000);
   },
   beforeDestroy() {
     window.removeEventListener("scroll", this.windowScroll);
